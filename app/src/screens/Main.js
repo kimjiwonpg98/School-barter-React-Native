00f15b71cb76efd33836ae53df704483d9f232b9
jsj <tw450497@gmail.com>
2021-09-16 20:00:05 +0900

    Add(sangjun/master): 메인페이지

---
+++ app/src/screens/Main.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect } from "react";
+import styled from "styled-components/native";
+import { ScrollView, Text } from "react-native";
+
+import { ReadyContext } from "../contexts";
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ theme }) => theme.background};
+  margin: 0;
+  padding: 0;
+`;
+
+const Main = ({ navigation }) => {
+  const { readyDispatch } = useContext(ReadyContext);
+
+  useEffect(() => {
+    readyDispatch.notReady();
+  }, []);
+
+  return (
+    <Container>
+      <ScrollView style={{ flex: 1, width: "100%" }}>
+        <Text>메인화면</Text>
+      </ScrollView>
+    </Container>
+  );
+};
+
+export default Main;
