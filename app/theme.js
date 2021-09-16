be978834dc1a10ee2ead2b8b9cdd03a000848a59
jsj <tw450497@gmail.com>
2021-09-16 19:45:08 +0900

    Add(sangjun/master): theme 파일 추가 사용시 수정필요

---
+++ app/theme.js
@@ -0,0 +1,64 @@
+const colors = {
+  white: "#ffffff",
+  black: "#000000",
+  grey_0: "#d5d5d5",
+  grey_1: "#a6a6a6",
+  grey_2: "#ecf0f1",
+  red: "#e84118",
+  blue: "#3679fe",
+};
+
+export const theme = {
+  background: colors.white,
+  text: colors.black,
+  errorText: colors.red,
+  focusText: colors.idu,
+  imageBackground: colors.idu,
+  label: colors.grey_1,
+  inputPlaceholder: colors.grey_1,
+  inputBorder: colors.grey_1,
+  errorText: colors.red,
+  buttonBackground: colors.idu,
+  buttonTitle: colors.white,
+  buttonUnfilledTitles: colors.red,
+  buttonUnfilledTitle: colors.blue,
+  spinnerBackground: colors.black,
+  spinnerIndicator: colors.white,
+  tabActiveColor: colors.idu,
+  tabInactiveColor: colors.grey_1,
+  buttonLogout: colors.idu,
+  inputDisabledBackground: colors.grey_0,
+  actionBackgroundColor: colors.idu2,
+  actionscircle: colors.idu9,
+  listBorder: colors.grey_0,
+  listTime: colors.grey_1,
+  listIcon: colors.black,
+  boardsButton: colors.idu4,
+  commentInput: colors.grey_2,
+  imageButtonBackground: colors.grey_1,
+  imageButtonIcon: colors.white,
+
+  // 헤더
+  headerBottomColor: colors.idu,
+  headerTintColor: colors.black,
+  headerIconColor: colors.idu7,
+
+  /* 헤더 타이틀 */
+  // 메인 타이틀
+  headerTitleBottomColor: colors.idu8,
+
+  // 마켓
+  marketBackground: colors.white,
+  marketFont: colors.black,
+  marketClickFont: colors.idu,
+  marketBorder: colors.grey_0,
+
+  // 상세 화면
+  profileImageColor: colors.idu8,
+
+  // 댓글
+  commentIdColor: colors.grey_1,
+
+  // Line Color
+  greyBottomLine: "#eceff1",
+};
